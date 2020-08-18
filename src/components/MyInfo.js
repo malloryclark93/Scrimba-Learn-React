4790b5d29be1b252e7cfafc797048f78d09fcd76
malloryclark93 <mallory_clark@live.com>
2020-08-18 14:13:42 -0600

    create components folder/ move MyInfo

---
+++ src/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+function MyInfo(){
+  return (
+    <div>
+      <h1>My Name is Mallory</h1>
+      <p>I am enjoying learning about React</p>
+      <p>Three Places I want to travel to are:</p>
+      <ul>
+        <li>San Diego</li>
+        <li>Austin</li>
+        <li>Barcelona</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo;
