b3172b8984d75ea8c2b505ed2377eb5c651bc42c
malloryclark93 <mallory_clark@live.com>
2020-08-18 14:10:56 -0600

    Page startup/MyInfo Component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function MyInfo(){
+  return (
+    <div>
+      <h1>My Name is Mallory</h1>
+      <p>I am enjoying learning about React</p>
+      <p>Three Places I want to travel to are:</p>
+      <ul>
+        <li>San Diego</li>
+        <li>Austin</li>
+        <li>Barcelona</li>
+      </ul>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(< MyInfo />, document.getElementById("root") 
+)
+
+
+
+
+export default MyInfo;
